3105793042600da8169cee933cfad8b2e3584fa6
Zeynalabdin <zeeynalabdin@gmail.com>
2021-11-30 18:19:11 +0400

    update img

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Animation from './components/Animation';
 function App() {
   return (
     <div className="App">
-      <Animation />
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import photo from '../photos/photo.jpg'
+import Animation from './Animation';
 
 const Home = () => {
 
